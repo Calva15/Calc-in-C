fc01315c94ef2078db870364ae78220135486c33
Carlos <alvarezcarlos232@gmail.com>
2025-01-16 11:59:31 -0600

    Version 0.2 Upload check README for more info

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # personalcalc
+
+# Version 0.2
+    - Added Checks to make sure proper Operators and Operrands are used
+    - Added comments to help with understanding the code
+
+# Checklist (Subject to Change).
+    - Add Loops to make it so you dont have to run the program again
--- calculator.c
+++ calculator.c
@@ -8,8 +8,37 @@ int main()
 // Ask User for input
 printf("Enter an operaator (+, -, *, /): ");
 scanf(" %c", &operator);
+
+// Validates Operator
+        while (1)
+        {
+          if (operator == '+' || operator == '-' ||
+                operator == '*' || operator == '/'){
+                    break;    
+            }  
+                else {
+                    printf("Invalid operator! Please enter a valid operator! (+, -, *, /): ");
+                    scanf(" %c", &operator); //Re-prompt until valid input is received
+
+                }
+
+        }
+        // Validates Operrands
+    while (1)
+    {
+     
 printf("Enter two operands: ");
-scanf("%lf %lf", &num1, &num2);
+if (scanf("%lf %lf", &num1, &num2) != 2) {
+    printf("Invalid input! Please enter two valid numbers. \n");
+        while(getchar() != '\n'); //clears input buffer
+    }
+        else {
+            break; //Exit loop if valid numbers are entered
+        }
+
+
+
+    }
 
 //perform the operation based on the operator
 switch (operator){
@@ -30,13 +59,13 @@ switch (operator){
      result = num1 / num2;
             else{
                     printf("Error! Division by zero .\n");
-                        return 1;
+                        return 1; // Exitsif Division is by Zero
 
             }
         break;
     default:
         printf("Error! Operator is not correct. \n");
-
+                return 1; //Exits if operator is invalid
 
 
 
